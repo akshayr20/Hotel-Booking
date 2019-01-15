7e4ea172e58725e895cf0d9d299f24447e0c76c3
akshayr20 <akshay.rajput5394@gmail.com>
2019-01-15 10:24:03 +0530

    added required components for the application, (header, sidebar, container and main section)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HotelViewComponent } from './hotel-view/hotel-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SidebarComponent,
+    HotelViewComponent
   ],
   imports: [
     BrowserModule
